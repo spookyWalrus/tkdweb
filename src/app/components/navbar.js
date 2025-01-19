adc8b2e2966eda67efa40cb5eddd8fae3e148f90
Zen Nakamura <zenzenzen@gmail.com>
2025-01-19 11:18:41 -0500

    Changed center logo to be round. Added topbar for member login and lang switcher. Modified navbar stylesheet for round logo, centered logo in mobile view and for topbar. Added variables stylesheet. Logo in navbar is now centered in mobile view. Nav burger now on right side.

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -12,112 +12,117 @@ export default function Navbar() {
   };
 
   return (
-    <nav
-      className="navbar is-fixed-top --bulma-primary-soft"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="navbar-brand">
-        <a href="/" className="navbar-logo">
-          <Image
-            src="/vercel.svg"
-            alt="Logo"
-            width="112"
-            height="28"
-            className="logo"
-          />
-        </a>
-        <a
-          role="button"
-          className={`navbar-burger ${navOpen ? "is-active" : ""}`}
-          aria-label="menu"
-          aria-expanded={navOpen ? "true" : "false"}
-          data-target="navbarBasicExample"
-          onClick={toggleMenu}
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
+    <div>
+      <div className="topBar">
+        <Link href="/" className="topBarLinks">
+          EN
+        </Link>
+        <Link href="/" className="topBarLinks">
+          Login
+        </Link>
       </div>
-
-      <div
-        id="navbarBasicExample"
-        className={`navbar-menu ${navOpen ? "is-active" : ""}`}
+      <nav
+        className="navbar is-fixed-top"
+        role="navigation"
+        aria-label="main navigation"
       >
-        <div className="navbar-container">
-          <div className="navbar-links-left navbar-start">
-            <div className="navbar-item has-dropdown is-hoverable">
-              <div className="navbar-link">About us</div>
-              <div className="navbar-dropdown">
-                <Link href="/" className="navbar-item navbar-item-link">
-                  Club history
-                </Link>
-                <Link href="/" className="navbar-item">
-                  Our instructors
-                </Link>
-              </div>
-            </div>
-            <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link">Courses</a>
-              <div className="navbar-dropdown">
-                <Link href="/" className="navbar-item">
-                  Adult course
-                </Link>
-                <Link href="/" className="navbar-item">
-                  Kids course
-                </Link>
-                <Link href="/" className="navbar-item">
-                  Schedule/ Calendar
-                </Link>
-                <Link href="/" className="navbar-item">
-                  Current news/events
-                </Link>
-              </div>
-            </div>
-          </div>
+        <div className="navbar-brand">
+          <a href="/" className="navbar-logo">
+            <Image
+              className="navbar-center-logo"
+              src="/logos/tkdBasic-web.png"
+              alt="Logo"
+              width="100"
+              height="100"
+            />
+          </a>
+          <a
+            role="button"
+            className={`navbar-burger ${navOpen ? "is-active" : ""}`}
+            aria-label="menu"
+            aria-expanded={navOpen ? "true" : "false"}
+            data-target="navbarBasicExample"
+            onClick={toggleMenu}
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
 
-          <div className="navbar-center">
-            <Link href="/">
-              <Image
-                className="navbar-center-logo"
-                src="/logos/rawdon-Rsymbol-colorNu.svg"
-                alt="Logo"
-                width="30"
-                height="30"
-              />
-            </Link>
-          </div>
-
-          <div className="navbar-links-right navbar-end">
-            <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link">About Taekwondo</a>
-              <div className="navbar-dropdown">
-                <Link href="/" className="navbar-item">
-                  History of Taekwondo
-                </Link>
-                <Link href="/" className="navbar-item">
-                  Principles of Taekwondo
-                </Link>
-                <Link href="/" className="navbar-item">
-                  Class conduct
-                </Link>
-                <Link href="/" className="navbar-item">
-                  Vocabulary / glossary
-                </Link>
+        <div
+          id="navbarBasicExample"
+          className={`navbar-menu ${navOpen ? "is-active" : ""}`}
+        >
+          <div className="navbar-container">
+            <div className="navbar-links-left navbar-start">
+              <div className="navbar-item has-dropdown is-hoverable">
+                <div className="navbar-link">About us</div>
+                <div className="navbar-dropdown">
+                  <Link href="/" className="navbar-item navbar-item-link">
+                    Club history
+                  </Link>
+                  <Link href="/" className="navbar-item">
+                    Our instructors
+                  </Link>
+                </div>
+              </div>
+              <div className="navbar-item has-dropdown is-hoverable">
+                <a className="navbar-link">Courses</a>
+                <div className="navbar-dropdown">
+                  <Link href="/" className="navbar-item">
+                    Adult course
+                  </Link>
+                  <Link href="/" className="navbar-item">
+                    Kids course
+                  </Link>
+                  <Link href="/" className="navbar-item">
+                    Schedule/ Calendar
+                  </Link>
+                  <Link href="/" className="navbar-item">
+                    Current news/events
+                  </Link>
+                </div>
               </div>
             </div>
-            <Link href="/" className="navbar-item">
-              Contact
-            </Link>
-            <div className="navbar-item-last">
-              <Link href="/" className="navbar-login navbar-item">
-                Members
+
+            <div className="navbar-center">
+              <Link href="/">
+                <Image
+                  className="navbar-center-logo"
+                  src="/logos/tkdBasic-web.png"
+                  alt="Logo"
+                  width="100"
+                  height="100"
+                />
+              </Link>
+            </div>
+
+            <div className="navbar-links-right navbar-end">
+              <div className="navbar-item has-dropdown is-hoverable">
+                <a className="navbar-link">About Taekwondo</a>
+                <div className="navbar-dropdown">
+                  <Link href="/" className="navbar-item">
+                    History of Taekwondo
+                  </Link>
+                  <Link href="/" className="navbar-item">
+                    Principles of Taekwondo
+                  </Link>
+                  <Link href="/" className="navbar-item">
+                    Class conduct
+                  </Link>
+                  <Link href="/" className="navbar-item">
+                    Vocabulary / glossary
+                  </Link>
+                </div>
+              </div>
+              <Link href="/" className="navbar-item">
+                Contact
               </Link>
             </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 }
