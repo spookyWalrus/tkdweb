fa7c7d57fef9a09dfed66ae4c575ac8d291e8705
Zen Nakamura <zenzenzen@gmail.com>
2025-03-08 14:47:53 -0500

    Temporary rename to force git to track case change

---
+++ src/components/langswitcher-temp.js
@@ -0,0 +1,30 @@
+import { useRouter, usePathname } from "../i18n/navigation";
+import { useLocale } from "next-intl";
+import { useState, useEffect } from "react";
+//added comment
+
+export default function LangSwitcher() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const currentLang = useLocale();
+  const [lang, setLang] = useState(currentLang);
+
+  useEffect(() => {
+    setLang(currentLang);
+  }, [currentLang]);
+
+  const langSetter = (e) => {
+    const newLang = e.target.value;
+    setLang(newLang);
+    router.push(pathname, { locale: newLang });
+  };
+
+  return (
+    <div className="select is-small">
+      <select value={lang} onChange={langSetter}>
+        <option value="en">ENG</option>
+        <option value="fr">FRA</option>
+      </select>
+    </div>
+  );
+}
