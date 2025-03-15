e8aca634c1c60b3db07c4109e6cef4b0ccd8bb1d
Zen Nakamura <zenzenzen@gmail.com>
2025-03-15 08:43:30 -0400

    Added missing locale directory. Added .gitignore for translation files

---
+++ locales/fr/fr-index.js
@@ -0,0 +1,11 @@
+import AboutBlock from "./AboutBlock.json";
+import HeroBlock from "./HeroBlock.json";
+import Metadata from "./Metadata.json";
+import Navbar from "./Navbar.json";
+
+export default {
+  AboutBlock,
+  HeroBlock,
+  Metadata,
+  Navbar,
+};
