3f8f8c90a316234261e8fa0bd5141ec831208024
Zen Nakamura <zenzenzen@gmail.com>
2025-03-22 19:54:35 -0400

    Added web form to send email of contact form, along with basic stylesheet. Modified some directory structure and links for contact page.

--- locales/en/en-index.js
+++ locales/en/en-index.js
@@ -2,10 +2,12 @@ import AboutBlock from "./AboutBlock.json";
 import HeroBlock from "./HeroBlock.json";
 import Metadata from "./Metadata.json";
 import Navbar from "./Navbar.json";
+import Contact from "./Contact.json";
 
 export default {
   AboutBlock,
   HeroBlock,
   Metadata,
   Navbar,
+  Contact,
 };
--- src/app/[locale]/Contact/page.js
+++ src/app/[locale]/Contact/page.js
@@ -1,7 +1,50 @@
-export default function Contact() {
+"use client";
+import { useState } from "react";
+import { useTranslations } from "next-intl";
+
+export default function ContactUs() {
+  const [status, setStatus] = useState("");
+  const t = useTranslations("Contact");
+
+  const submitMail = async (e) => {
+    e.preventDefault();
+    try {
+      setStatus("sending mail...");
+      const formData = new FormData(e.target);
+      formData.append("access_key", "289101d9-a5e8-4394-8ccb-5c53d469a895");
+
+      const res = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData,
+      });
+      if (res.ok) {
+        setStatus("Email sent successfully");
+        e.target.reset();
+      } else {
+        setStatus("Failed to send email");
+        throw new Error("Failed to submit the form");
+      }
+    } catch (error) {
+      // console.log("Error sending email: ", error);
+      setStatus("error on submit");
+    }
+  };
+
   return (
-    <div>
-      <p className="is-size-3 is-family-monospace ">Contact us </p>
+    <div className="formTop">
+      <h2>{t("ContactUs")}</h2>
+      <form onSubmit={submitMail}>
+        <label htmlFor="name">{t("Name")}</label>
+        <input type="text" name="name" required />
+        <label htmlFor="email">{t("Email")}</label>
+        <input type="email" name="email" required />
+        <label htmlFor="subject">{t("Subject")}</label>
+        <input type="text" name="subject" required />
+        <label htmlFor="message">{t("Message")}</label>
+        <textarea name="message" required />
+        <button type="submit">{t("Send")}</button>
+      </form>
+      {status && <p>{status}</p>}
     </div>
   );
 }
