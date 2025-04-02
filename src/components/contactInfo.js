0b46921c54340616367fe314d7851a34abc05ff6
Zen Nakamura <zenzenzen@gmail.com>
2025-04-02 11:51:44 -0400

    Added translation entry points for contactInfo heading and content.

--- src/components/contactInfo.js
+++ src/components/contactInfo.js
@@ -4,29 +4,31 @@ import {
   faPhone,
   faLocationDot,
 } from "@fortawesome/free-solid-svg-icons";
+import { useTranslations } from "next-intl";
 
 const ContactInfo = () => {
+  const t = useTranslations("ContactInfo");
   return (
     <div>
       <div className="icon-box">
         <span className="icon isBlue">
           <FontAwesomeIcon icon={faEnvelope} />
         </span>
-        <span className="contactBold">Email</span>
+        <span className="contactBold">{t("Email")}</span>
         <span className="contactInfo">tkdccs@gmail.com</span>
       </div>
       <div className="icon-box">
         <span className="icon isBlue">
           <FontAwesomeIcon icon={faPhone} />
         </span>
-        <span className="contactBold">Phone</span>
+        <span className="contactBold">{t("Phone")}</span>
         <span className="contactInfo">090-123-4567</span>
       </div>
       <div className="icon-box">
         <span className="icon isBlue">
           <FontAwesomeIcon icon={faLocationDot} />
         </span>
-        <span className="contactBold">Address</span>
+        <span className="contactBold">{t("Address")}</span>
         <div className="contactInfo">
           <p>1234 Main Street</p>
           <p>City, State, 12345</p>
