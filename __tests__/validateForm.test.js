dc91f5327ef56554c798a7b70560b8998e1557d6
Zen Nakamura <zenzenzen@gmail.com>
2025-04-05 10:06:58 -0400

    Added test for contact form validation. Moved contact form validation function as a utility function. Ran tests on validationForm.js

---
+++ __tests__/validateForm.test.js
@@ -0,0 +1,16 @@
+import { _testExports } from "@/utilities/validateForm";
+const { validateForm } = _testExports;
+
+describe("checking validateForm function", () => {
+  const t = (key) => key;
+
+  test("email error", () => {
+    const errors = validateForm({ email: "dude" }, t);
+    expect(errors).toHaveProperty("email", "EmailError");
+  });
+
+  test("name error", () => {
+    const errors = validateForm({ name: "a" }, t);
+    expect(errors).toHaveProperty("name", "NameError");
+  });
+});
---
+++ src/utilities/validateForm.js
@@ -0,0 +1,15 @@
+export const validateForm = (data, t) => {
+  const newErrors = {};
+  if (!data.name || data.name.length < 2) {
+    newErrors.name = t("NameError");
+  }
+  if (!data.email || !/\S+@\S+\.\S+/.test(data.email)) {
+    newErrors.email = t("EmailError");
+  }
+  if (!data.message || data.message.length < 10) {
+    newErrors.message = t("MessageError");
+  }
+  return newErrors;
+};
+
+export const _testExports = { validateForm };
