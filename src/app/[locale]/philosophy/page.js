3b9895096b845f4653fb3baf03a9fab96af2cdc0
Zen Nakamura <zenzenzen@gmail.com>
2025-04-19 10:35:03 -0400

    Added full translated content points for philosopy page. Adjusted stylings for Korean font.

--- src/app/[locale]/philosophy/page.js
+++ src/app/[locale]/philosophy/page.js
@@ -12,26 +12,88 @@ export default function Philosophy() {
         <div className="inlineImage">
           <div className="underTrans">
             <p className="eulFont">여의</p>
-            <p>(Ye Ui)</p>
+            <p>(Yeo Ui)</p>
           </div>
           <div className="description">
             <h4 className="has-text-weight-semibold">
-              {t("Phil-courtesy-header")}
+              {t("Phil-courtesy.Title")}
             </h4>
-            <p>{t("Phil-courtesy1")}</p>
-            <p>{t("Phil-courtesy2")}</p>
-            <p>{t("Phil-courtesy3")}</p>
+            <p>{t("Phil-courtesy.1")}</p>
+            <p>{t("Phil-courtesy.2")}</p>
+            <p>{t("Phil-courtesy.3")}</p>
+          </div>
+        </div>
+        <br />
+        <div className="inlineImage bodyTextMarginTop">
+          <div className="underTrans">
+            <p className="eulFont">염치</p>
+            <p>(Yeom Chi)</p>
+          </div>
+          <div className="description">
+            <h4 className="has-text-weight-semibold">
+              {t("Phil-integrity.Title")}
+            </h4>
+            <p>{t("Phil-integrity.1")}</p>
+            <p>{t("Phil-integrity.2")}</p>
+          </div>
+        </div>
+        <br />
+        <div className="inlineImage bodyTextMarginTop">
+          <div className="underTrans">
+            <p className="eulFont">인내</p>
+            <p>(In Nae)</p>
+          </div>
+          <div className="description">
+            <h4 className="has-text-weight-semibold">
+              {t("Phil-perseverance.Title")}
+            </h4>
+            <p>{t("Phil-perseverance.1")}</p>
+            <p>{t("Phil-perseverance.2")}</p>
+          </div>
+        </div>
+        <br />
+        <div className="inlineImage bodyTextMarginTop">
+          <div className="underTrans">
+            <p className="eulFont">극기</p>
+            <p>(Geuk Gi)</p>
+          </div>
+          <div className="description">
+            <h4 className="has-text-weight-semibold">
+              {t("Phil-selfControl.Title")}
+            </h4>
+            <p>{t("Phil-selfControl.1")}</p>
+            <p>{t("Phil-selfControl.2")}</p>
+          </div>
+        </div>
+        <br />
+        <div className="inlineImageCustom bodyTextMarginTop">
+          <div className="underTrans has-text-left">
+            <p className="eulFont">백절불굴</p>
+            <p className="adjustLeft">(Baekjul Boolgool)</p>
+          </div>
+          <br />
+          <div>
+            <h4 className="has-text-weight-semibold">
+              {t("Phil-indSpirit.Title")}
+            </h4>
+            <p>{t("Phil-indSpirit.1")}</p>
+            <p>{t("Phil-indSpirit.2")}</p>
           </div>
         </div>
 
-        <br />
-        {/* <p>{t("Phil-integrity")}</p>
-        <br />
-        <p>{t("Phil-perseverance")}</p>
-        <br />
-        <p>{t("Phil-selfControl")}</p>
-        <br />
-        <p>{t("Phil-indomitableSpirit")}</p> */}
+        <div className="bodyTextMarginTop">
+          <p>{t("AddPhilTKD.1")}</p>
+          <p>{t("AddPhilTKD.2a")}</p>
+          <p>{t("AddPhilTKD.2b")}</p>
+          <p>{t("AddPhilTKD.2c")}</p>
+          <br />
+          <p>{t("AddPhilTKD.3")}</p>
+          <br />
+          <div>
+            <p className="eulFont">삶의 방식</p>
+            <p>(Salm-ui Bangsik)</p>
+          </div>
+        </div>
       </div>
     </div>
   );
