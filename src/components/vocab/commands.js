592ab0a3cf3b403c186ae41aefb568a419633b18
Zen Nakamura <zenzenzen@gmail.com>
2025-04-22 13:53:22 -0400

    Finalized pages. Finalized adding translation points for pages. Modified some styling for pages. Modified dev and build script to DL from two translation accounts in package.json

---
+++ src/components/vocab/commands.js
@@ -0,0 +1,50 @@
+import { useTranslations } from "next-intl";
+
+export default function Commands() {
+  const t = useTranslations("Vocab");
+
+  return (
+    <div className="section noLPadding">
+      <div className="has-text-left">
+        <h3>{t("Commands.header")}</h3>
+      </div>
+      <div className="columns is-mobile is-multiline ">
+        <div className="columnCel">
+          <p>{t("Commands.Ready")}</p>
+          <p className="eulFontSM">준비</p>
+          <p>&quot;Junbei&quot;</p>
+        </div>
+        <div className="columnCel">
+          <p>{t("Commands.Begin")}</p>
+          <p className="eulFontSM">시작</p>
+          <p>&quot;Sijak&quot;</p>
+        </div>
+        <div className="columnCel">
+          <p>{t("Commands.Stop")}</p>
+          <p className="eulFontSM">그만</p>
+          <p>&quot;Geuman&quot;</p>
+        </div>
+        <div className="columnCel">
+          <p>{t("Commands.Attention")}</p>
+          <p className="eulFontSM">차렷</p>
+          <p>&quot;Charyeot&quot;</p>
+        </div>
+        <div className="columnCel">
+          <p>{t("Commands.Bow")}</p>
+          <p className="eulFontSM">경례</p>
+          <p>&quot;Kyung-nyeh&quot;</p>
+        </div>
+        <div className="columnCel">
+          <p>{t("Commands.ThankYouFormal")}</p>
+          <p className="eulFontSM">감사합니다</p>
+          <p>&quot;Gamsamnida&quot;</p>
+        </div>
+        <div className="columnCel">
+          <p>{t("Commands.ThankYouCasual")}</p>
+          <p className="eulFontSM">고마워요</p>
+          <p>&quot;Gomawoyo&quot;</p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/vocab/people.js
@@ -0,0 +1,40 @@
+import { useTranslations } from "next-intl";
+
+export default function People() {
+  const t = useTranslations("Vocab2");
+
+  return (
+    <div className="section noLPadding">
+      <div className="has-text-left">
+        <h3>{t("People.header")}</h3>
+      </div>
+      <div className="columns is-mobile is-multiline ">
+        <div className="columnCel">
+          <p>{t("People.Student")}</p>
+          <p className="eulFontSM">교사님</p>
+          <p>&quot;Jae-ja&quot;</p>
+        </div>
+        <div className="columnCel">
+          <p>{t("People.1+Instructor")}</p>
+          <p className="eulFontSM">교사님</p>
+          <p>&quot;Kyosa-nim&quot;</p>
+        </div>
+        <div className="columnCel">
+          <p>{t("People.4+Master")}</p>
+          <p className="eulFontSM">범님사</p>
+          <p>&quot;Sabeom-nim&quot;</p>
+        </div>
+        <div className="columnCel">
+          <p>{t("People.GrandMaster")}</p>
+          <p className="eulFontSM">관장님</p>
+          <p>&quot;Gwangjang-nim&quot;</p>
+        </div>
+        <div className="columnCel">
+          <p>{t("People.HighGrandMaster")}</p>
+          <p className="eulFontSM">대사범님</p>
+          <p>&quot;Daesabeom-nim&quot;</p>
+        </div>
+      </div>
+    </div>
+  );
+}
