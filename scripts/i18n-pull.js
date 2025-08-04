96f0d28353db32af81171ac1b45e3d312cfd44b9
Zen Nakamura <zenzenzen@gmail.com>
2025-08-04 07:51:19 -0400

    Modified node script to check for api keys when running gitHub actions. Added steps to check for api key in .yml file

--- scripts/i18n-pull.js
+++ scripts/i18n-pull.js
@@ -10,5 +10,11 @@ function runI18nexusPull(apiKey) {
   execSync(`I18NEXUS_API_KEY="${apiKey}" i18nexus pull`, { stdio: "inherit" });
 }
 
+// Use CI environment variables if available, else fall back to .env
+const API_KEY1 =
+  process.env.I18NEXUS_API_KEY1 || process.env.GITHUB_I18NEXUS_API_KEY1;
+const API_KEY2 =
+  process.env.I18NEXUS_API_KEY2 || process.env.GITHUB_I18NEXUS_API_KEY2;
+
 runI18nexusPull(process.env.I18NEXUS_API_KEY1);
 runI18nexusPull(process.env.I18NEXUS_API_KEY2);
