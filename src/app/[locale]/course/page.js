6e6dbc8ad1c528ad77dc2a80db723f4d40e58f41
Zen <zenzenzen@gmail.com>
2026-09-12 14:28:10 -0400

    Updated content for 2026-27 year. Modified some tables and content. Modified translations for the page. Created script so translations are loaded from local file @ /TKD/transSources-json when compiled locally

--- src/app/[locale]/course/page.js
+++ src/app/[locale]/course/page.js
@@ -44,20 +44,28 @@ export default function CourseFees() {
         </div>
         <div>
           <div>{mapSpaces(t("Schedule.Top"))}</div>
-          <SessionStart />
+          {/* <SessionStart /> */}
+          <div>
+            <h5>Session 1 : Sep 15 2026</h5>
+            <h5>Session 2 : Dec 08 2026</h5>
+            <h5>Session 3 : Mar 03 2026</h5>
+          </div>
         </div>
         <br />
         <div>
           <p>
-            {t("Download")}
+            {t("Schedule.Download")}
             <a
-              href="https://oqcbfcberasludcppgxy.supabase.co/storage/v1/object/public/TKDcalendar/calendar2025.pdf?download"
+              href="https://oqcbfcberasludcppgxy.supabase.co/storage/v1/object/public/TKDcalendar/2026-27-calendar.jpg"
+              target="_blank"
+              rel="noopener noreferrer"
               className="button is-link dlcalendarbutton"
             >
               {seasonCalc()}
             </a>
           </p>
         </div>
+
         <div className="bodyTextMarginTop">
           <h4>{t("Schedule.TableHeader")}</h4>
           <table className="table is-striped">
@@ -77,10 +85,12 @@ export default function CourseFees() {
                   <p className="noBold">{t("Schedule.BeginnerDesc")}</p>
                 </th>
                 <td>
-                  18:30 <br />
+                  18:30 - 19:30
+                  <br />
                 </td>
                 <td>
-                  18:30 <br />
+                  18:30 - 19:30
+                  <br />
                 </td>
                 <td>13:00</td>
               </tr>
@@ -91,106 +101,97 @@ export default function CourseFees() {
                   <p className="noBold">{t("Schedule.AdvancedDesc")}</p>
                 </th>
                 <td>
-                  18:30 <br />
+                  18:30 - 20h
+                  <br />
                 </td>
                 <td>
-                  18:30 <br />
+                  18:30 - 20h
+                  <br />
                 </td>
-                <td>13:00</td>
+                <td>13:00 - 15h</td>
               </tr>
               <tr>
                 <th>{t("Schedule.Belts")}</th>
                 <td>
-                  19:45 <br />
+                  18:30 - 20:30
+                  <br />
                 </td>
                 <td>
-                  19:45 <br />
+                  18:30 - 20:30
+                  <br />
                 </td>
-                <td>13:00</td>
+                <td>13:00 - 15h</td>
               </tr>
             </tbody>
           </table>
           <div>{mapSpaces(t("Schedule.Comment"))}</div>
         </div>
+
+        <div className="bodyTextMarginTop">
+          <h5>{t("Streams.Header")}</h5>
+          <div className="content">
+            <ul>
+              <li>
+                <p className="strongBold">{t("Streams.RecTitle")}</p>
+                <ul>
+                  <li>{t("Streams.RecDesc")}</li>
+                  <p className="strongBoldMed">{t("Streams.RecListTitle")}</p>
+                  <ul>
+                    <li>{t("Streams.RecList1")}</li>
+                    <li>{t("Streams.RecList2")}</li>
+                    <li>{t("Streams.RecList3")}</li>
+                    <li>{t("Streams.RecList4")}</li>
+                    <li>{t("Streams.RecList5")}</li>
+                  </ul>
+                </ul>
+              </li>
+              <li>
+                <p className="strongBold">{t("Streams.CompTitle")}</p>
+                <ul>
+                  <li>{t("Streams.CompDesc")}</li>
+                  <ul>
+                    <li>{t("Streams.CompList1")}</li>
+                    <li>{t("Streams.CompList2")}</li>
+                    <li>{t("Streams.CompList3")}</li>
+                    <li>{t("Streams.CompList4")}</li>
+                    <li>{t("Streams.CompList5")}</li>
+                  </ul>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+
         <div className="bodyTextMarginTop">
           <h4>{t("Fees.Title")}</h4>
           <div>
-            <p>{t("Fees.FeeTable")}</p>
+            <p>{t("Fees.InPerson")}</p>
           </div>
-          {/* <table className="table is-striped">
-            <thead className="thead">
-              <tr>
-                <th></th>
-                <th className="tableHead">{t("Fees.SessionCost")}</th>
-                <th className="tableHead">
-                  {t("Fees.FamilyRate")}
-                  <br />
-                  <p className="noBold">{t("Fees.FamilyRateDesc")}</p>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th>
-                  {t("Fees.RecCat")}
-                  <br />
-                  <p className="noBold">{t("Fees.RecCatDesc")}</p>
-                </th>
-                <td>85$</td>
-                <td>Family price$</td>
-              </tr>
-              <tr>
-                <th>
-                  {t("Fees.CompCat")}
-                  <br />
-                  <p className="noBold">{t("Fees.CompCatDesc")}</p>
-                </th>
-                <td>100$ </td>
-                <td>Family price$</td>
-              </tr>
-              <tr>
-                <th>
-                  {t("Fees.AdvCat")}
-                  <br />
-                  <p className="noBold">{t("Fees.AdvCatDesc")}</p>
-                </th>
-                <td>100$ </td>
-                <td>Family price$</td>
-              </tr>
-              <tr>
-                <th>
-                  {t("Fees.BBeltCat")}
-                  <br />
-                  <p className="noBold">{t("Fees.BBeltCatDesc")}</p>
-                </th>
-                <td>120$ </td>
-                <td>Family price$</td>
-              </tr>
-            </tbody>
-          </table> */}
         </div>
         <div>
           <h4 className="bodyTextMarginTop">{t("Fees.AddCos")}</h4>
           <div>
-            <h5 className="underline">{t("Fees.Eqp")}</h5>
-            <p>{t("Fees.Dobuk")} (Dobuk) : 40$</p>
-            <p>{t("Fees.Protection")} : 100$</p>
+            <h6>{t("Fees.Eqp")}</h6>
+            <p>{t("Fees.Dobuk")} (Dobuk)</p>
+            <p>{t("Fees.Protection")}</p>
             <br />
             <p>{t("Fees.GearPurchase")}</p>
           </div>
         </div>
         <div className="bodyTextMarginTop">
-          <h5 className="underline">{t("Fees.Fees")}</h5>
-          <p>{t("Fees.QTKDclr")} : 40$</p>
-          <p>{t("Fees.QTKDblk")} : 50$</p>
+          <h6>{t("Fees.Fees")}</h6>
+          <p>{t("Fees.QTKDw")} : 20$</p>
+          <p>{t("Fees.QTKDclr")} : 30$</p>
+          <p>{t("Fees.QTKDblk")} : 35$</p>
         </div>
         <div className="bodyTextMarginTop">
-          <h5 className="underline">{t("Fees.Exams")}</h5>
-          <p>{t("Fees.ExamClrBelt")} : 30$</p>
-          <p>{t("Fees.ExamBlkBelt")} : 30$</p>
+          <h6>{t("Fees.Exams")}</h6>
+          {/* <p>{t("Fees.ExamClrBelt")} : 30$</p>
+          <p>{t("Fees.ExamBlkBelt")} : 30$</p> */}
           <div>{mapSpaces(t("Fees.ExamComment"))}</div>
         </div>
-        <div className="bodyTextMarginTop">
+        <div>
+          <h4 className="bodyTextMarginTop">{t("Fees.Payments")}</h4>
           <div>{mapSpaces(t("FinalComment"))}</div>
           <h6>{mapSpaces(t("PayInfo"))}</h6>
         </div>
